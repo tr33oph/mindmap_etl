9bfc3d844e826b59d35a4ad3f5321296a7dbacb2
liuhaojie <liuhaojie@seclover.com>
2020-05-24 15:22:02 +0800

    update example

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mindmap_etl",
-    version="0.1.2",
+    version="0.1.3",
     keywords=["mindmap", "ETL", 'etl', 'graph'],
     description="使用 .xmmap 思维导图文件构建模型和ETL导入逻辑，转换为图数据库需要的结构。",
     long_description="",
